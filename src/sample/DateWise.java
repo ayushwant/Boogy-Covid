a8bbcf4424868197fa71bbd577234bc297a6d55d
ayushwant <ayushwant912@gmail.com>
2020-11-01 10:58:55 +0530

    added date in dateWise

--- src/sample/DateWise.java
+++ src/sample/DateWise.java
@@ -189,7 +189,7 @@ public class DateWise implements Initializable {
                 int[] deathcase=new int[36];
 
                 Calendar calendar=Calendar.getInstance();
-                String date=calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-"+calendar.get(Calendar.DATE);
+                String date=calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-0"+calendar.get(Calendar.DATE);
                 try {
                     BufferedReader br = new BufferedReader(new FileReader("indiaHistoryJSON.json"));
                     indiaHistory response = gson.fromJson(br, indiaHistory.class);
